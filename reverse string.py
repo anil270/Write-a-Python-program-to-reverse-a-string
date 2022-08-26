0d7e4d0c0da7b8bee0969b6f6b41f2f08fdf9aa9
Anil Kumar <102414580+anil270@users.noreply.github.com>
2022-08-27 01:14:44 +0530

    Add files via upload

---
+++ reverse string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Write a Python program to reverse a string.
+
+# In[65]:
+
+
+def reverse():
+    x=input("Enter the String :")
+    return x[::-1]
+txt=reverse()
+print("reverse string :",txt)
+
